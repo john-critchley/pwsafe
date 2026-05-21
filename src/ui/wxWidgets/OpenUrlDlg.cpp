e5bfc117532b2fdbc16c71163c1699f5a8f22b4a
Debian <admin@kelp.critchley.biz>
2026-05-21 06:52:41 +0000

    Allow default URL for WebDAV open dialog

--- src/ui/wxWidgets/OpenUrlDlg.cpp
+++ src/ui/wxWidgets/OpenUrlDlg.cpp
@@ -15,6 +15,7 @@
 #include <wx/button.h>
 #include <wx/sizer.h>
 #include <wx/stattext.h>
+#include <wx/utils.h>
 
 BEGIN_EVENT_TABLE(OpenUrlDlg, wxDialog)
   EVT_BUTTON(wxID_OK, OpenUrlDlg::OnOkClick)
@@ -43,7 +44,10 @@ void OpenUrlDlg::CreateControls(const wxArrayString &recentUrls)
   m_urlCtrl = new wxComboBox(this, wxID_ANY, wxEmptyString,
                               wxDefaultPosition, wxSize(420, -1),
                               recentUrls, wxCB_DROPDOWN);
-  if (!recentUrls.IsEmpty())
+  wxString defaultUrl;
+  if (wxGetEnv(wxT("DEFAULT_URL"), &defaultUrl) && !defaultUrl.IsEmpty())
+    m_urlCtrl->SetValue(defaultUrl);
+  else if (!recentUrls.IsEmpty())
     m_urlCtrl->SetValue(recentUrls[0]);
   row->Add(m_urlCtrl, 1, wxEXPAND);
 
